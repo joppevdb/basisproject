79fa284ebf1add5924bb8929adf515987fe4d558
joppevandenbroeck <71765408+joppevdb@users.noreply.github.com>
2022-11-15 10:32:33 +0100

    Update bier.py

--- app/bier.py
+++ app/bier.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://localhost.tiangolo.com",
+    "http://127.0.0.1:5500"
+    "https://bier-service-joppevdb.cloud.okteto.net"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # class voor item
 class Bier(BaseModel):
